9d3982ac2fea318cc9030890a8b9e5dea632724d
JSLim <socier@gmail.com>
2022-10-19 13:46:32 +0700

    리스트 이모지 추가

--- ptha_bot.py
+++ ptha_bot.py
@@ -6,11 +6,12 @@ import telegram
 # TELEGRAM 관련 설정
 TELEGRAM_TOKEN = '5700322476:AAGnGmckNe0jbBwhnNMt3apxsNpVyCL3lsY'   # ptha_bot
 TELEGRAM_CHAT_ID = -1001809120134	# PTHA 영업 채널
+#TELEGRAM_CHAT_ID = 26288969	# JSLim
 
 bot = telegram.Bot(token = TELEGRAM_TOKEN)
 
 def send_telegram(msg, nopreveiw='true'):
-	bot.sendMessage(chat_id = TELEGRAM_CHAT_ID, parse_mode='HTML', text=msg, disable_web_page_preview=nopreveiw)
+	bot.sendMessage(chat_id = TELEGRAM_CHAT_ID, parse_mode='HTML', text=msg, disable_web_page_preview=False)
 	print (msg)
 
 def read_sql(sql):
@@ -30,12 +31,22 @@ def prod_info():
 	run_date = str(df.run_date[0])
 	run_time = str(df.run_time[0])
 	
-	msg  = '----------------------------------\n'
-	msg += 'Order Transfer Status\n'
-	msg += '----------------------------------\n'
-	msg += f'{run_status} {run_date[0:4]}/{run_date[4:6]}/{run_date[6:8]} {run_time[0:2]}:{run_time[2:4]}:{run_time[4:6]}\n'
+	if run_status == 'Fail':
+		emoji = '⛔⛔⛔'
+	else:
+		emoji = '😊'
 
-	msg += '----------------------------------\n'
+	#msg  = '----------------------------------\n'
+	msg = f'Order Transfer <b>{run_status}</b> {emoji}\n'
+	#msg += '----------------------------------\n'
+
+	#if run_status == 'Fail':
+	#	msg += "<a href='https://s3.us-west-2.amazonaws.com/secure.notion-static.com/81e7cd92-3bd7-4915-8f0b-e81514f2b796/fail.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220929%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220929T080629Z&X-Amz-Expires=86400&X-Amz-Signature=9c7c1d64fed04def89334463c3d3804727a752c84d2bc82fe26353f85865ea4f&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22fail.png%22&x-id=GetObject'>Fail</a>"
+	#else:
+	#	msg += run_status
+
+	msg += f' {run_date[0:4]}/{run_date[4:6]}/{run_date[6:8]} {run_time[0:2]}:{run_time[2:4]}:{run_time[4:6]}\n'
+	#msg += '----------------------------------\n'
 
 	send_telegram(msg)
 
